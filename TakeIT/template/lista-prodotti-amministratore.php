a6b1aa10cf2e1491bc7c8829906276bd32250915
Manuel Baldoni <manuel.baldoni04@gmail.com>
2024-12-30 22:57:51 +0100

    Fix css lista prodotti

--- TakeIT/template/lista-prodotti-amministratore.php
+++ TakeIT/template/lista-prodotti-amministratore.php
@@ -7,50 +7,149 @@
 		Crea un nuovo prodotto
 	</a>
 
-	<form>
+	<form method="GET">
 		<label>
 			<span aria-hidden="true" class="fa-solid fa-magnifying-glass"></span>
 			<span class="fa-sr-only">Cerca per codice o nome</span>
-			<input name="search" type="search" placeholder="Cerca per codice o nome">
+			<input name="q" type="search" placeholder="Cerca per codice o nome" value="<?php echo $_GET["q"] ?? ""; ?>">
 		</label>
-		<select aria-label="Ordina per" name="order">
-			<option value="" disabled selected>Ordina per:</option>
-			<option value="price-asc">Prezzo crescente</option>
-			<option value="price-desc">Prezzo decrescente</option>
-			<option value="reviews">Numero di recensioni</option>
+		<select aria-label="Ordina per" name="ordine">
+			<option value="" disabled <?php if(!isset($_GET["ordine"])): echo "selected"; endif; ?>>Ordina per:</option>
+			<option value="prezzo ASC" <?php if(($_GET["ordine"] ?? "") == "prezzo ASC"): echo "selected"; endif; ?>>Prezzo crescente</option>
+			<option value="prezzo DESC" <?php if(($_GET["ordine"] ?? "") == "prezzo DESC"): echo "selected"; endif; ?>>Prezzo decrescente</option>
+			<option value="dataCreazione ASC" <?php if(($_GET["ordine"] ?? "") == "dataCreazione ASC"): echo "selected"; endif; ?>>Data creazione crescente</option>
+			<option value="dataCreazione DESC" <?php if(($_GET["ordine"] ?? "") == "dataCreazione DESC"): echo "selected"; endif; ?>>Data creazione decrescente</option>
 		</select>
-	</form>
+		<label>
+			<input type="submit" value=""/>
+			Cerca
+		</label>
 
-	<button id="btn-mobile-filters-opener">
-		<span aria-hidden="true" class="fa-solid fa-filter"></span>
-		<span class="fa-sr-only">Filtra prodotti</span>
-		Filtra prodotti
-	</button>
-	<!-- FILTRI -->
-	<form id="mobile-filters">
-		<button id="btn-mobile-filters-closer" type="button" aria-label="Chiudi i filtri">
-			<span aria-hidden="true" class="fa-solid fa-xmark"></span>
-			<span class="fa-sr-only">Chiudi i filtri</span>
-		</button>
+		<label>
+			<input id="btn-mobile-filters-opener" type="button" />
+			<span aria-hidden="true" class="fa-solid fa-filter"></span>
+			<span class="fa-sr-only">Filtra prodotti</span>
+			Filtra prodotti
+		</label>
+		<!-- FILTRI -->
+		<section id="mobile-filters">
+			<button id="btn-mobile-filters-closer" type="button" aria-label="Chiudi i filtri">
+				<span aria-hidden="true" class="fa-solid fa-xmark"></span>
+				<span class="fa-sr-only">Chiudi i filtri</span>
+			</button>
 
-		<section>
-			<h3>Tipologia</h3>
-			<ul>
-				<?php foreach($templateParams["tipologie_prodotti"] as $tipologia_prodotto): ?>
+			<section>
+				<h3>Tipologia</h3>
+				<ul>
+					<?php foreach($templateParams["tipologie_prodotto"] as $tipologia_prodotto): ?>
+						<li>
+							<label>
+								<input name="tipologie_prodotto[]" type="checkbox" value="<?php echo $tipologia_prodotto["nome"]; ?>" <?php if(in_array($tipologia_prodotto["nome"], $_GET["tipologie_prodotto"] ?? [])): echo "checked"; endif; ?>>
+								<?php echo $tipologia_prodotto["nome"]; ?>
+							</label>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</section>
+
+			<section>
+				<h3>Marca</h3>
+				<ul>
+					<?php foreach($templateParams["marche"] as $marca): ?>
+						<li>
+							<label>
+								<input name="marche[]" type="checkbox" value="<?php echo $marca["codice"]; ?>" <?php if(in_array($marca["codice"], $_GET["marche"] ?? [])): echo "checked"; endif; ?>>
+								<?php echo $marca["titolo"]; ?>
+							</label>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</section>
+
+			<section>
+				<h3>Recensioni</h3>
+
+				<ul>
 					<li>
 						<label>
-							<input type="checkbox" value="">
-							<?php echo $tipologia_prodotto["nome"]; ?>
+							<input name="recensioni[]" type="checkbox" value="5" <?php if(in_array("5", $_GET["recensioni"] ?? [])): echo "checked"; endif; ?> />
+							<span class="fa-sr-only">5 stelle</span>
+							<ul>
+								<li><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span></li>
+							</ul>
 						</label>
 					</li>
-				<?php endforeach; ?>
-			</ul>
-		</section>
+					<li>
+						<label>
+							<input name="recensioni[]" type="checkbox" value="4" <?php if(in_array("4", $_GET["recensioni"] ?? [])): echo "checked"; endif; ?> />
+							<span class="fa-sr-only">4 stelle</span>
+							<ul>
+								<li><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-regular fa-star"></span></li>
+							</ul>
+						</label>
+					</li>
+					<li>
+						<label>
+							<input name="recensioni[]" type="checkbox" value="3" <?php if(in_array("3", $_GET["recensioni"] ?? [])): echo "checked"; endif; ?> />
+							<span class="fa-sr-only">3 stelle</span>
+							<ul>
+								<li><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span></li>
+							</ul>
+						</label>
+					</li>
+					<li>
+						<label>
+							<input name="recensioni[]" type="checkbox" value="2" <?php if(in_array("2", $_GET["recensioni"] ?? [])): echo "checked"; endif; ?> />
+							<span class="fa-sr-only">2 stelle</span>
+							<ul>
+								<li><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span></li>
+							</ul>
+						</label>
+					</li>
+					<li>
+						<label>
+							<input name="recensioni[]" type="checkbox" value="1" <?php if(in_array("1", $_GET["recensioni"] ?? [])): echo "checked"; endif; ?> />
+							<span class="fa-sr-only">1 stella</span>
+							<ul>
+								<li><span class="fa-solid fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span></li>
+							</ul>
+						</label>
+					</li>
+					<li>
+						<label>
+							<input name="recensioni[]" type="checkbox" value="0" <?php if(in_array("0", $_GET["recensioni"] ?? [])): echo "checked"; endif; ?> />
+							<span class="fa-sr-only">1 stella</span>
+							<ul>
+								<li><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span><span class="fa-regular fa-star"></span></li>
+							</ul>
+						</label>
+					</li>
+				</ul>
+			</section>
 
-		<section>
-			<button id="btn-mobile-filters-applier" type="submit">
-				Applica filtri
-			</button>
+			<section>
+				<h3>Prezzo</h3>
+
+				<ul>
+					<li>
+						<label>
+							Minimo:
+							<input type="number" min="0" step="0.01" name="prezzo_min" value="<?php echo $_GET["prezzo_min"] ?? ""; ?>" placeholder="0.00"/>
+						</label>
+					</li>
+					<li>
+						<label>
+							Massimo:
+							<input type="number" max="<?php echo $templateParams["prodotti_max_price"]["prezzo"]; ?>" step="0.01" name="prezzo_max" value="<?php echo $_GET["prezzo_max"] ?? ""; ?>" placeholder="<?php echo $templateParams["prodotti_max_price"]["prezzo"]; ?>"/>
+						</label>
+					</li>
+				</ul>
+			</section>
+
+			<label>
+				<span aria-hidden="true" class="fa-sr-only">Applica filtri</span>
+				<input type="submit" value="Applica filtri">
+			</label>
 		</section>
 	</form>
 </header>
@@ -97,7 +196,7 @@
                         <li>
                             <label>
                                 <span class="fa-sr-only">Quantità del prodotto</span>
-                                <input type="number" name="quantita" value="1" aria-label="Quantità del prodotto" readonly/>
+                                <input type="number" name="quantita" value="<?php echo $prodotto["quantita"]; ?>" aria-label="Quantità del prodotto" readonly/>
                             </label>
                         </li>
                         <li>
@@ -111,7 +210,7 @@
             </section>
 			<footer>
 				<a href="gestisci-prodotto.php?action=1&codiceProdotto=<?php echo $prodotto['codice']; ?>">Modifica</a>
-			    <a href="gestisci-prodotto.php?action=2&codiceProdotto=<?php echo $prodotto['codice']; ?>">Elimina</a>
+				<a href="gestisci-prodotto.php?action=2&codiceProdotto=<?php echo $prodotto['codice']; ?>">Elimina</a>
 			</footer>
 		</article>
     <?php endforeach; ?>
