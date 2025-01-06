a5886583998fa2a50dbcdfbae9bfe945e4415ded
Manuel Baldoni <manuel.baldoni04@gmail.com>
2025-01-06 11:08:23 +0100

    fix css prodotti amministratore and topologie prodotto

--- TakeIT/template/lista-tipologie-prodotto.php
+++ TakeIT/template/lista-tipologie-prodotto.php
@@ -20,37 +20,39 @@
 	</form>
 </header>
 
-<?php if(count($templateParams["tipologie_prodotto"]) == 0): ?>
-	<p>Non ci sono tipologie di prodotto</p>
-<?php else: 
-	$tipologie_prodotto = $templateParams["tipologie_prodotto"]; 
-?>
-	<?php foreach($tipologie_prodotto as $tipologia_prodotto): ?>
-		<article>
-			<header>
-				<h2><?php echo htmlspecialchars($tipologia_prodotto["nome"]); ?></h2>
-			</header>
-			
-			<section>
-				<h3>Descrizione:</h3>
-				<p class="descrizione"><?php echo htmlspecialchars($tipologia_prodotto["descrizione"]); ?></p>
-			</section>
-			
-			<section>
-				<h3>Caratteristiche:</h3>
-				<ul class="caratteristiche">
-					<?php foreach($tipologia_prodotto["caratteristiche"] as $caratteristica): ?>
-						<li>
-							<span><?php echo htmlspecialchars($caratteristica["nome"]); ?></span>
-						</li>
-					<?php endforeach; ?>
-				</ul>
-			</section>
-			
-			<footer>
-				<a href="gestisci-tipologia.php?action=2&nome=<?php echo $tipologia_prodotto["nome"]; ?>">Modifica</a>
-				<a href="gestisci-tipologia.php?action=3&nome=<?php echo $tipologia_prodotto["nome"]; ?>">Elimina</a>
-			</footer>
-		</article>
-	<?php endforeach; ?>
-<?php endif; ?>
+<section>
+	<?php if(count($templateParams["tipologie_prodotto"]) == 0): ?>
+		<p>Non ci sono tipologie di prodotto</p>
+	<?php else: 
+		$tipologie_prodotto = $templateParams["tipologie_prodotto"]; 
+	?>
+		<?php foreach($tipologie_prodotto as $tipologia_prodotto): ?>
+			<article>
+				<header>
+					<h2><?php echo htmlspecialchars($tipologia_prodotto["nome"]); ?></h2>
+				</header>
+				
+				<section>
+					<h3>Descrizione:</h3>
+					<p class="descrizione"><?php echo htmlspecialchars($tipologia_prodotto["descrizione"]); ?></p>
+				</section>
+				
+				<section>
+					<h3>Caratteristiche:</h3>
+					<ul class="caratteristiche">
+						<?php foreach($tipologia_prodotto["caratteristiche"] as $caratteristica): ?>
+							<li>
+								<span><?php echo htmlspecialchars($caratteristica["nome"]); ?></span>
+							</li>
+						<?php endforeach; ?>
+					</ul>
+				</section>
+				
+				<footer>
+					<a href="gestisci-tipologia.php?action=2&nome=<?php echo $tipologia_prodotto["nome"]; ?>">Modifica</a>
+					<a href="gestisci-tipologia.php?action=3&nome=<?php echo $tipologia_prodotto["nome"]; ?>">Elimina</a>
+				</footer>
+			</article>
+		<?php endforeach; ?>
+	<?php endif; ?>
+</section>
